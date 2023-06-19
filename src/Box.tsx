bae970a2e2b49e16f6cf631938a7c595a3a6b84a
Alejandro Medina <5almero5@gmail.com>
2023-06-19 14:38:31 -0600

    Initial commit

---
+++ src/Box.tsx
@@ -0,0 +1,29 @@
+import * as THREE from "three";
+import { useRef, useState } from "react";
+import { useFrame, ThreeElements } from "@react-three/fiber";
+
+function Box(props: ThreeElements["mesh"]) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef<THREE.Mesh>(null!);
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((_state, delta) => (mesh.current.rotation.x += delta));
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(_event) => setActive(!active)}
+      onPointerOver={(_event) => setHover(true)}
+      onPointerOut={(_event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
+export default Box;
